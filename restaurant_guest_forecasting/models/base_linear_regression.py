c135ff505520e7ba9c6f3763ec0de84fcb3e1d3b
Nienke Posthumus <nienkeposthumus@macbookpro.home>
2025-05-27 13:03:53 +0200

    Added doc strings and type hints

--- restaurant_guest_forecasting/models/base_linear_regression.py
+++ restaurant_guest_forecasting/models/base_linear_regression.py
@@ -3,19 +3,36 @@ from sklearn.linear_model import LinearRegression
 import sys
 import os
 from sklearn.metrics import mean_squared_error
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 
+                                             '..', 'data')))
 from train_test_split import train_val_test_data
 from normalization import normalize_data
 
-
-def load_linear_regression():
+def load_linear_regression() -> None:
+    """
+    Loads and trains a linear regression model on restaurant guest data.
+    
+    This function:
+    - Loads training, validation, and test data.
+    - Removes columns starting with 'art_'.
+    - Combines validation and test sets for final evaluation.
+    - Normalizes the data.
+    - Splits the data into features (X) and target (y).
+    - Trains a linear regression model.
+    - Evaluates the model using Mean Squared Error (MSE).
+    - Prints model accuracy and sorted feature importance.
+    
+    Returns:
+        None
+    """
     full_restaurant_df = train_val_test_data()
     regression_df = []
 
     # Dropping article columns for train, val and test dataframes
     for dataframe in full_restaurant_df:
-        dropped_cols_df = dataframe.drop(columns=[col for col in dataframe.columns if 
-                                col.startswith('art_')])
+        dropped_cols_df = dataframe.drop(columns=[col for col in 
+                                                  dataframe.columns if 
+                                                  col.startswith('art_')])
         regression_df.append(dropped_cols_df)
 
     train, val, test = regression_df
@@ -45,17 +62,18 @@ def load_linear_regression():
     # Mean squared error to obtain the accuracy
     accuracy = 1 - mean_squared_error(y_test, y_pred)
     print(f"Base linear regression model accuracy: {accuracy}")
-
-    # Checking feature importance
+    
     # Coefficients correspond to feature importance
     importance = model.coef_
 
-    # Assuming X_train is a DataFrame, you can pair feature names with their coefficients
+    # Assuming X_train is a DataFrame, you can pair feature names with their 
+    # coefficients
     feature_importance = dict(zip(X_train.columns, importance))
 
-    # Print or sort
+    # Print features, sorted based on importance
     print("---feature importance---")
-    for feature, coef in sorted(feature_importance.items(), key=lambda x: abs(x[1]), reverse=True):
+    for feature, coef in sorted(feature_importance.items(), key=lambda 
+                                x: abs(x[1]), reverse=True):
         print(f"{feature}: {coef:.4f}")
 
 if __name__ == "__main__":

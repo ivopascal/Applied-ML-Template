684aa6d503759b200720e5620c46ab3ea42a0c71
ThisIsPiclasso <github.buckwheat412@passfwd.com>
2026-05-19 16:23:13 +0000

    align dataclass getitem to work with pytorch and design factory pattern

--- MVL-AI-Classifier/data/dataclass.py
+++ MVL-AI-Classifier/data/dataclass.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+import torch
 import pandas as pd
 from PIL import Image
 
@@ -6,9 +7,17 @@ PATCH_SIZE = 256
 
 
 class DataClass(Dataset):
-    def __init__(self, parquet_file, split="train"):
+    def __init__(self, parquet_file, view_configuration: dict, split="train"):
+        super().__init__()
+        self.view_configuration = view_configuration
+        self.split = split
         self.df = pd.read_parquet(parquet_file)
         self.df = self.df[self.df["split"] == split].reset_index(drop=True)
+
+        self.preprocessors = {
+            view_name: config_["preprocessor"]
+            for view_name, config_ in view_configuration.items()
+        }
 
     def __len__(self):
         return len(self.df)
@@ -21,7 +30,12 @@ class DataClass(Dataset):
         return img.crop((x, y, x + patch_size, y + patch_size))
 
     def __getitem__(self, idx):
-        # Need to change this, Pytorch expects different format, needs a tensor so we need to call preprocessors from here, but that requires the setup of the view factory structure first.
         item = self.df.iloc[idx]
-        img = Image.open(item["path"]).convert("RGB")
-        return self.get_patch(img)
+        image_ = Image.open(item["path"]).convert("RGB")
+        label = item["category"]
+
+        view_outputs = {}
+        for view_name, preprocessor in self.preprocessors.items():
+            # this will run through all preprocessors, convert the outputs into a torch tensor and add the result back in a dictionary
+            view_outputs[view_name] = torch.from_numpy(preprocessor(image_)).float()
+        return {"views": view_outputs, "label": torch.tensor(label, dtype=torch.long)}

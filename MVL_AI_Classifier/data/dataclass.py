4492ea3b298fcba6c7cbee624730529c8c52f602
ThisIsPiclasso <github.buckwheat412@passfwd.com>
2026-05-20 14:46:57 +0000

    change patchting from centre patch to deterministic random patches during training

--- MVL_AI_Classifier/data/dataclass.py
+++ MVL_AI_Classifier/data/dataclass.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import torch
 import pandas as pd
 from PIL import Image
+import numpy as np
 
 PATCH_SIZE = 256
 
@@ -22,16 +23,28 @@ class DataClass(Dataset):
     def __len__(self):
         return len(self.df)
 
-    def get_patch(self, img: Image.Image):
-        width, height = img.size
+    def _get_patch(self, image_: Image.Image, idx: int):
+        width, height = image_.size
         patch_size = PATCH_SIZE
-        x = (width - patch_size) // 2
-        y = (height - patch_size) // 2
-        return img.crop((x, y, x + patch_size, y + patch_size))
+        if self.split in ["val", "test"]:
+            x = (width - patch_size) // 2
+            y = (height - patch_size) // 2
+        else:
+            seed = int(self.current_epoch * 997 + idx)
+            rng = np.random.default_generator(np.random.PCG64(seed))
+
+            max_x = width - self.patch_size
+            max_y = height - self.patch_size
+
+            x = rng.integers(0, max_x) if max_x > 0 else 0
+            y = rng.integers(0, max_y) if max_y > 0 else 0
+
+        return image_.crop((x, y, x + patch_size, y + patch_size))
 
     def __getitem__(self, idx):
         item = self.df.iloc[idx]
         image_ = Image.open(item["path"]).convert("RGB")
+        image_ = self._get_patch(image_, idx)
         label = item["category"]
 
         view_outputs = {}
